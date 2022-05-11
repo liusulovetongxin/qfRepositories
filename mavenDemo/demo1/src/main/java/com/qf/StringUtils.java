deef671f44e4f9372a2ef61931b6935f804be7e6
liusulovetongxin <a3092147280@gmail.com>
2022-05-11 16:25:46 +0800

    maven测试

---
+++ mavenDemo/demo1/src/main/java/com/qf/StringUtils.java
@@ -0,0 +1,19 @@
+package com.qf;
+
+/**
+ * @author Administrator
+ * @version V1.0
+ * @Project mavenDemo
+ * @Package com.qf
+ * @Description:
+ * @Date 2022/5/10 14:43
+ */
+
+public class StringUtils {
+    public static boolean isEmpty(String str){
+        if (str==null){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ mavenDemo/demo1/src/test/java/com/qf/test/Test.java
@@ -0,0 +1,13 @@
+package com.qf.test;
+
+/**
+ * @author Administrator
+ * @version V1.0
+ * @Project mavenDemo
+ * @Package com.qf.test
+ * @Description:
+ * @Date 2022/5/10 16:10
+ */
+
+public class Test {
+}
